2c46529f8347bd3c73bfae02d77f892308ef3477
OrchidBones <104790039+OrchidBones@users.noreply.github.com>
2024-07-25 18:58:12 +0800

    Add files via upload

---
+++ pinyin-converter/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(()=>{
+    $("button#do").click(()=>{
+        const ori = $("textarea#imp").val();
+        const target = pinyinUtil.getPinyin(ori);
+        $("textarea#exp").val(target);
+    });
+
+    $("button#clear").click(()=>{
+        $("textarea#imp").val("");
+        $("textarea#emp").val("");
+    })
+});
